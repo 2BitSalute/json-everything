238abbee44742536707e79b9afac58b120531670
Greg Dennis <gregsdennis@yahoo.com>
2024-07-12 22:44:35 +1200

    better integration into the main lib

--- src/JsonSchema.Tests/LearnJsonEverythingTestFixture.cs
+++ src/JsonSchema.Tests/LearnJsonEverythingTestFixture.cs
@@ -1,5 +1,4 @@
-﻿#if DEBUG
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text.Json.Nodes;
 using NUnit.Framework;
@@ -18,6 +17,7 @@ public class LearnJsonEverythingTestFixture
 	}
 
 	[TestCaseSource(nameof(TestCases))]
+	[Ignore("Dev purposes only, supports learning site")]
 	public void RunCases(JsonNode data)
 	{
 		var result = Run(data.AsObject());
@@ -51,4 +51,3 @@ public class LearnJsonEverythingTestFixture
 		return schema.Evaluate(instance, options);
 	}
 }
-#endif
